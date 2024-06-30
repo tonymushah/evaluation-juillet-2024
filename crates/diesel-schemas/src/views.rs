a0f3b9b9debec2464eec9be8cd6b1a4001b297cc
tonymushah <tonyramanantsalama@gmail.com>
2024-06-30 17:58:10 +0300

    Added `create_v_bien` migration

--- crates/diesel-schemas/src/views.rs
+++ crates/diesel-schemas/src/views.rs
@@ -24,3 +24,18 @@ diesel::table! {
         date_entree -> Timestamp,
     }
 }
+
+diesel::table! {
+    v_bien (id_bien, id_type_bien, proprietaire, id_type_bien_commission) {
+        id_bien -> Uuid,
+        nom -> Text,
+        region -> Uuid,
+        description -> Text,
+        proprietaire -> Text,
+        id_type_bien -> Uuid,
+        loyer -> Numeric,
+        id_type_bien_commission -> Uuid,
+        commission -> Numeric,
+        nom_type_bien -> Text
+    }
+}
