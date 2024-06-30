9a1839da5b19556dad500f45f5192f9fbda61002
tonymushah <tonyramanantsalama@gmail.com>
2024-06-30 16:54:01 +0300

    added `create_bien` migration

--- crates/diesel-schemas/src/schema.rs
+++ crates/diesel-schemas/src/schema.rs
@@ -1,6 +1,17 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    bien (id_bien) {
+        id_bien -> Uuid,
+        nom -> Text,
+        region -> Uuid,
+        description -> Text,
+        proprietaire -> Text,
+        type_bien -> Nullable<Uuid>,
+    }
+}
+
+diesel::table! {
     proprietaire (telephone) {
         telephone -> Text,
         nom -> Text,
@@ -30,9 +41,13 @@ diesel::table! {
     }
 }
 
+diesel::joinable!(bien -> proprietaire (proprietaire));
+diesel::joinable!(bien -> region (region));
+diesel::joinable!(bien -> type_bien (type_bien));
 diesel::joinable!(type_bien_commission -> type_bien (type_bien));
 
 diesel::allow_tables_to_appear_in_same_query!(
+    bien,
     proprietaire,
     region,
     type_bien,
