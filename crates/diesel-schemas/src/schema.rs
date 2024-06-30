99f1f5955731306f776d55465abd9fd9c20ab18d
tonymushah <tonyramanantsalama@gmail.com>
2024-06-30 11:10:47 +0300

    added `create_proprietaire` migration

--- crates/diesel-schemas/src/schema.rs
+++ crates/diesel-schemas/src/schema.rs
@@ -1,8 +1,20 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    proprietaire (telephone) {
+        telephone -> Text,
+        nom -> Text,
+    }
+}
+
+diesel::table! {
     region (id_region) {
         id_region -> Uuid,
         nom -> Text,
     }
 }
+
+diesel::allow_tables_to_appear_in_same_query!(
+    proprietaire,
+    region,
+);
