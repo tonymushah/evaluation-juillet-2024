77f5400267bd79712667320cd58d771c8a322d76
tonymushah <tonyramanantsalama@gmail.com>
2024-06-30 18:26:59 +0300

    added `create_location` migration

--- crates/diesel-schemas/src/schema.rs
+++ crates/diesel-schemas/src/schema.rs
@@ -28,6 +28,16 @@ diesel::table! {
 }
 
 diesel::table! {
+    location (id_location) {
+        id_location -> Uuid,
+        bien -> Uuid,
+        client -> Text,
+        date_debut -> Timestamp,
+        date_fin -> Nullable<Timestamp>,
+    }
+}
+
+diesel::table! {
     proprietaire (telephone) {
         telephone -> Text,
         nom -> Text,
@@ -61,12 +71,15 @@ diesel::joinable!(bien -> proprietaire (proprietaire));
 diesel::joinable!(bien -> region (region));
 diesel::joinable!(bien -> type_bien (type_bien));
 diesel::joinable!(bien_loyer -> bien (bien));
+diesel::joinable!(location -> bien (bien));
+diesel::joinable!(location -> client (client));
 diesel::joinable!(type_bien_commission -> type_bien (type_bien));
 
 diesel::allow_tables_to_appear_in_same_query!(
     bien,
     bien_loyer,
     client,
+    location,
     proprietaire,
     region,
     type_bien,
---
+++ migrations/2024-06-30-151008_create_location/up.sql
@@ -0,0 +1,8 @@
+-- Your SQL goes here
+create table location (
+    id_location UUID PRIMARY KEY default gen_random_uuid(),
+    bien UUID REFERENCES bien(id_bien) NOT null,
+    client TEXT REFERENCES client(email) NOT null,
+    date_debut TIMESTAMP NOT NULL default now(),
+    date_fin TIMESTAMP
+);
