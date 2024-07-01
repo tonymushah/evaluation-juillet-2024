ce23d70912427d76c4cbc5cfca44914bedb5dc47
tonymushah <tonyramanantsalama@gmail.com>
2024-07-01 20:53:49 +0300

    Added 'mushah-proto-build'

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,28 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { kitRoutes } from 'vite-plugin-kit-routes';
 import type { KIT_ROUTES } from './app/lib/ROUTES';
+import { spawn } from 'node:child_process';
 
 export default defineConfig({
 	plugins: [
+		{
+			name: 'mushah-proto-build',
+			async buildStart(options) {
+				return new Promise((res, rej) => {
+					const proto = spawn('pnpm', ['proto:all']);
+					proto.on('error', rej);
+					proto.stdout.on('data', (e: Buffer) => {
+						console.log(e.toString());
+					});
+					proto.stderr.on('data', (e) => {
+						rej(new Error(e));
+					});
+					proto.on('close', () => {
+						res();
+					});
+				});
+			}
+		},
 		sveltekit(),
 		kitRoutes<KIT_ROUTES>({
 			routes_path: 'app/routes',
