1a1850263e8e3adcc4cc8be6dec3786d167397e3
tonymushah <tonyramanantsalama@gmail.com>
2024-07-02 02:13:01 +0300

    some generateTokenHandler fixes

--- app/lib/generateTokenHandler.server.ts
+++ app/lib/generateTokenHandler.server.ts
@@ -2,39 +2,45 @@ import { redirect, type Cookies } from '@sveltejs/kit';
 
 // place files you want to import through the `$lib` alias in this folder.
 export default function generateTokenHandler(KEY: string, path?: string) {
-	return {
-		hasToken(cookies: Cookies): boolean {
-			return cookies.get(KEY) != undefined;
-		},
-		getToken(cookies: Cookies): string {
-			const token = cookies.get(KEY);
-			if (token == undefined) {
-				throw new Error('Token not found');
-			} else {
-				return token;
-			}
-		},
-		getTokenWithCallback(cookies: Cookies, callback: () => never): string {
-			try {
-				return this.getToken(cookies);
-			} catch (error) {
-				callback();
-			}
-		},
-		getTokenOrRedirect(cookies: Cookies, path: string): string {
-			return this.getTokenWithCallback(cookies, () => {
-				redirect(301, path);
-			});
-		},
-		setToken(cookies: Cookies, value: string) {
-			cookies.set(KEY, value, {
-				path: path ?? '/'
-			});
-		},
-		deleteToken(cookies: Cookies) {
-			cookies.delete(KEY, {
-				path: path ?? '/'
-			});
+	function hasToken(cookies: Cookies): boolean {
+		return cookies.get(KEY) != undefined;
+	}
+	function getToken(cookies: Cookies): string {
+		const token = cookies.get(KEY);
+		if (token == undefined) {
+			throw new Error('Token not found');
+		} else {
+			return token;
 		}
+	}
+	function getTokenWithCallback(cookies: Cookies, callback: () => never): string {
+		try {
+			return getToken(cookies);
+		} catch (error) {
+			callback();
+		}
+	}
+	function getTokenOrRedirect(cookies: Cookies, path: string): string {
+		return getTokenWithCallback(cookies, () => {
+			redirect(301, path);
+		});
+	}
+	function setToken(cookies: Cookies, value: string) {
+		cookies.set(KEY, value, {
+			path: path ?? '/'
+		});
+	}
+	function deleteToken(cookies: Cookies) {
+		cookies.delete(KEY, {
+			path: path ?? '/'
+		});
+	}
+	return {
+		hasToken,
+		getToken,
+		getTokenWithCallback,
+		getTokenOrRedirect,
+		setToken,
+		deleteToken
 	};
 }
--- app/lib/generateTokenHandler.ts
+++ app/lib/generateTokenHandler.ts
@@ -2,21 +2,22 @@ import { goto } from '$app/navigation';
 import { getContext, setContext } from 'svelte';
 
 export default function generateTokenHandler(KEY: string) {
+	function getToken(): string {
+		const context = getContext<string | undefined>(KEY);
+		if (context != undefined) {
+			return context;
+		} else {
+			throw new Error(`${KEY} context is undefined`);
+		}
+	}
 	return {
 		setToken(value: string): string {
 			return setContext(KEY, value);
 		},
-		getToken(): string {
-			const context = getContext<string | undefined>(KEY);
-			if (context != undefined) {
-				return context;
-			} else {
-				throw new Error(`${KEY} context is undefined`);
-			}
-		},
+		getToken,
 		getTokenOrRedirect(path: string): string {
 			try {
-				return this.getToken();
+				return getToken();
 			} catch (error) {
 				goto(path);
 				throw error;
