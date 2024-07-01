96e0f2cc9459fc7fd5b3d76a892f909d3b20485c
tonymushah <tonyramanantsalama@gmail.com>
2024-07-02 02:23:27 +0300

    Added `+layout.svelte` init contexts

--- app/routes/proprio/(logged)/+layout.svelte
+++ app/routes/proprio/(logged)/+layout.svelte
@@ -1,1 +1,13 @@
+<script lang="ts">
+	import { setToken } from '$lib/proprio/token.client';
+	import { initGRPCClientContext } from '$lib/rpc_client.context';
+	import type { LayoutServerData } from './$types';
+
+	export let data: LayoutServerData;
+	initGRPCClientContext({
+		baseUrl: data.backUrl
+	});
+	setToken(data.token);
+</script>
+
 <slot />
