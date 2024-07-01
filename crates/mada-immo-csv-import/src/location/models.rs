2bc17e40850d3bc652531d05be6ab4bf37950545
tonymushah <tonyramanantsalama@gmail.com>
2024-07-01 14:41:46 +0300

    Added `InsertLocation`

--- crates/mada-immo-csv-import/src/location/models.rs
+++ crates/mada-immo-csv-import/src/location/models.rs
@@ -1,6 +1,27 @@
 use diesel::prelude::*;
-use diesel_schemas::tables::client;
+use diesel_schemas::tables::{client, location};
 use time::PrimitiveDateTime;
+use uuid::Uuid;
+
+#[derive(Debug, Clone, Insertable)]
+#[diesel(table_name = location)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct InsertLocation {
+    pub bien: String,
+    pub client: String,
+    pub date_debut: PrimitiveDateTime,
+    pub date_fin: PrimitiveDateTime,
+}
+
+impl InsertLocation {
+    pub fn insert(&self, con: &mut PgConnection) -> QueryResult<Uuid> {
+        use self::location::dsl::*;
+        diesel::insert_into(location)
+            .values(self)
+            .returning(id_location)
+            .get_result(con)
+    }
+}
 
 #[derive(Debug, Clone, Insertable)]
 #[diesel(table_name = client)]
