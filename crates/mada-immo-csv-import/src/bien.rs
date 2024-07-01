7db5cbaadcef7a9dbd4612f6f8ef2bbbe705a4a6
tonymushah <tonyramanantsalama@gmail.com>
2024-07-01 11:45:54 +0300

    added `CsvBien`

--- crates/mada-immo-csv-import/src/bien.rs
+++ crates/mada-immo-csv-import/src/bien.rs
@@ -1,2 +1,100 @@
 pub mod models;
 pub mod utils;
+
+use bigdecimal::BigDecimal;
+use diesel::{prelude::*, PgConnection, QueryResult};
+use models::{InsertBien, InsertBienLoyer, InsertProprietaire, InsertRegion, InsertTypeBien};
+use serde::Deserialize;
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct CSVBien {
+    pub reference: String,
+    pub nom: String,
+    #[serde(alias = "Description")]
+    pub description: String,
+    #[serde(alias = "Type")]
+    pub type_: String,
+    pub region: String,
+    #[serde(alias = "loyer mensuel")]
+    pub loyer: BigDecimal,
+    #[serde(alias = "Proprietaire")]
+    pub proprietaire: String,
+}
+
+impl CSVBien {
+    pub fn insert(&self, con: &mut PgConnection) -> QueryResult<()> {
+        let reg = InsertRegion::get_by_nom_or_insert(self.region.clone(), con)?;
+        let type_ = InsertTypeBien::get_by_nom_or_insert(self.type_.clone(), con)?;
+        InsertProprietaire::insert(self.proprietaire.clone(), con)?;
+        {
+            use diesel_schemas::tables::bien::dsl::*;
+            diesel::insert_into(bien)
+                .values(InsertBien {
+                    id_bien: self.reference.clone(),
+                    nom: self.nom.clone(),
+                    region: reg,
+                    description: self.description.clone(),
+                    proprietaire: self.proprietaire.clone(),
+                    type_bien: type_,
+                })
+                .execute(con)?;
+        }
+        InsertBienLoyer {
+            bien: self.reference.clone(),
+            valeur: self.loyer.clone(),
+        }
+        .insert(con)?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use diesel::{
+        r2d2::{ConnectionManager, Pool, PooledConnection},
+        PgConnection,
+    };
+    use dotenvy::dotenv;
+    use std::{env, fs::File, io::BufReader};
+
+    use csv::Reader;
+
+    use super::CSVBien;
+
+    pub type DbPool = Pool<ConnectionManager<PgConnection>>;
+
+    //pub type DbPoolConnection = PooledConnection<ConnectionManager<PgConnection>>;
+
+    fn etablish_connection() -> DbPool {
+        dotenv().ok();
+
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        let manager = ConnectionManager::<PgConnection>::new(database_url);
+
+        Pool::builder()
+            .build(manager)
+            .expect("Failed to create a pool.")
+    }
+
+    #[test]
+    fn parse_test() {
+        let reader =
+            BufReader::new(File::open("../../data/Donnees-csv-saison3 - Biens.csv").unwrap());
+        let mut reader = Reader::from_reader(reader);
+        for bien in reader.deserialize::<CSVBien>().flatten() {
+            println!("{:?}", bien)
+        }
+    }
+    #[test]
+    fn insert_test() {
+        let reader =
+            BufReader::new(File::open("../../data/Donnees-csv-saison3 - Biens.csv").unwrap());
+        let pool = etablish_connection();
+        let mut con = pool.get().unwrap();
+
+        let mut reader = Reader::from_reader(reader);
+        for bien in reader.deserialize::<CSVBien>().flatten() {
+            bien.insert(&mut con).unwrap();
+        }
+    }
+}
--- crates/mada-immo-csv-import/src/bien/models.rs
+++ crates/mada-immo-csv-import/src/bien/models.rs
@@ -1,1 +1,143 @@
+use bigdecimal::BigDecimal;
+use diesel::prelude::*;
+use diesel_schemas::tables::{bien, bien_loyer, proprietaire, region, type_bien};
+use uuid::Uuid;
 
+#[derive(Debug, Clone, Insertable, Selectable, Queryable)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[diesel(table_name = bien)]
+pub struct InsertBien {
+    pub id_bien: String,
+    pub nom: String,
+    pub region: Uuid,
+    pub description: String,
+    pub proprietaire: String,
+    pub type_bien: Uuid,
+}
+
+#[derive(Debug, Clone, Insertable)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[diesel(table_name = bien_loyer)]
+pub struct InsertBienLoyer {
+    pub bien: String,
+    pub valeur: BigDecimal,
+}
+
+impl InsertBienLoyer {
+    pub fn insert(&self, con: &mut PgConnection) -> QueryResult<Uuid> {
+        use self::bien_loyer::dsl::*;
+        diesel::insert_into(bien_loyer)
+            .values(self)
+            .returning(id_bien_loyer)
+            .get_result(con)
+    }
+}
+
+#[derive(Debug, Clone, Insertable)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[diesel(table_name = type_bien)]
+pub struct InsertTypeBien {
+    pub id_type_bien: Uuid,
+    pub designation: String,
+}
+
+impl InsertTypeBien {
+    fn insert_by_nom(nom_: String, con: &mut PgConnection) -> QueryResult<Uuid> {
+        use self::type_bien::dsl::*;
+        let to_insert = Self {
+            id_type_bien: Uuid::new_v4(),
+            designation: nom_,
+        };
+        diesel::insert_into(type_bien)
+            .values(to_insert)
+            .returning(id_type_bien)
+            .get_result(con)
+    }
+    fn get_by_nom(nom_: String, con: &mut PgConnection) -> QueryResult<Uuid> {
+        use self::type_bien::dsl::*;
+        type_bien
+            .filter(designation.eq(nom_))
+            .select(id_type_bien)
+            .get_result(con)
+    }
+    pub fn get_by_nom_or_insert(nom: String, con: &mut PgConnection) -> QueryResult<Uuid> {
+        let res = Self::get_by_nom(nom.clone(), con);
+        if let Ok(id) = &res {
+            Ok(*id)
+        } else if let Err(diesel::result::Error::NotFound) = &res {
+            Self::insert_by_nom(nom, con)
+        } else {
+            res
+        }
+    }
+}
+
+#[derive(Debug, Clone, Insertable)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[diesel(table_name = proprietaire)]
+pub struct InsertProprietaire {
+    pub telephone: String,
+    pub nom: String,
+}
+
+impl InsertProprietaire {
+    pub fn insert(telephone_: String, con: &mut PgConnection) -> QueryResult<()> {
+        use self::proprietaire::dsl::*;
+        let inner = proprietaire
+            .filter(telephone.eq(&telephone_))
+            .select(telephone)
+            .get_result::<String>(con);
+        if inner.is_ok() {
+            Ok(())
+        } else if let Err(diesel::result::Error::NotFound) = &inner {
+            diesel::insert_into(proprietaire)
+                .values(Self {
+                    telephone: telephone_,
+                    nom: Default::default(),
+                })
+                .execute(con)?;
+            Ok(())
+        } else {
+            inner.map(|_| {})
+        }
+    }
+}
+
+#[derive(Debug, Clone, Insertable)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[diesel(table_name = region)]
+pub struct InsertRegion {
+    pub id_region: Uuid,
+    pub nom: String,
+}
+
+impl InsertRegion {
+    fn insert_by_nom(nom_: String, con: &mut PgConnection) -> QueryResult<Uuid> {
+        use self::region::dsl::*;
+        let to_insert = Self {
+            id_region: Uuid::new_v4(),
+            nom: nom_,
+        };
+        diesel::insert_into(region)
+            .values(to_insert)
+            .returning(id_region)
+            .get_result(con)
+    }
+    fn get_by_nom(nom_: String, con: &mut PgConnection) -> QueryResult<Uuid> {
+        use self::region::dsl::*;
+        region
+            .filter(nom.eq(nom_))
+            .select(id_region)
+            .get_result(con)
+    }
+    pub fn get_by_nom_or_insert(nom: String, con: &mut PgConnection) -> QueryResult<Uuid> {
+        let res = Self::get_by_nom(nom.clone(), con);
+        if let Ok(id) = &res {
+            Ok(*id)
+        } else if let Err(diesel::result::Error::NotFound) = &res {
+            Self::insert_by_nom(nom, con)
+        } else {
+            res
+        }
+    }
+}
